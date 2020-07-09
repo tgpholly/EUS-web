76063a91dd33669a1f3627fe4b5d09d265ce8000
tgpethan <ethan@pyra-studios.net>
2020-07-09 14:20:11 +0100

    Change fetchStats to comply with new space api changes

--- js/fetchStats.js
+++ js/fetchStats.js
@@ -4,42 +4,42 @@ function fetchStatsFromAPI() {
 }
 
 function updateFileStats() {
-    let xhr = new XMLHttpRequest();
-    let pngLabel = document.getElementById("png"),
+    let xhr = new XMLHttpRequest(),
+        pngLabel = document.getElementById("png"),
         jpgLabel = document.getElementById("jpg"),
         gifLabel = document.getElementById("gif"),
         spaceBar = document.getElementById("spaceBar"),
         percentLabel = document.getElementById("percent"),
-        usedspaceLabel = document.getElementById("usedspace");
+        usedspaceLabel = document.getElementById("usedspace"),
+        totalspaceLabel = document.getElementById("totalspace");
     xhr.open("GET", "/api/get-stats?f=1&s=1", true);
     xhr.onload = (e) => {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            const apiData = JSON.parse(xhr.responseText);
-            console.log(apiData);
-            const perper = (apiData.space.mb / 1887436.8 * 100); // I still need to add disk space to the api
+            const apiData = JSON.parse(xhr.responseText),
+                  percentage = (apiData.space.usage.mb / apiData.space.total.mbvalue * 100);
             pngLabel.innerHTML = apiData.files.png;
             jpgLabel.innerHTML = apiData.files.jpg + apiData.files.jpeg;
             gifLabel.innerHTML = apiData.files.gif;
-            spaceBar.value = perper;
-            percentLabel.innerHTML = `${perper.toFixed(2)}% Usage`;
-            usedspaceLabel.innerHTML = apiData.space.string;
+            spaceBar.value = percentage;
+            percentLabel.innerHTML = `${percentage.toFixed(2)}% Usage`;
+            usedspaceLabel.innerHTML = apiData.space.usage.string;
+            totalspaceLabel.innerHTML = apiData.space.total.string;
         } else console.error(e);
     }
     xhr.send(null);
 }
 
 function getServerInfo() {
-    let xhr2 = new XMLHttpRequest();
-    let vernumLabel = document.getElementById("vernum"),
+    let xhr = new XMLHttpRequest(),
+        vernumLabel = document.getElementById("vernum"),
         instaLabel = document.getElementById("insta");
-    xhr2.open("GET", "/api/get-info", true);
-    xhr2.onload = (e) => {
-        if (xhr2.readyState === 4 && xhr2.status === 200) {
-            const apiData = JSON.parse(xhr2.responseText);
-            console.log(apiData);
+    xhr.open("GET", "/api/get-info", true);
+    xhr.onload = (e) => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const apiData = JSON.parse(xhr.responseText);
             vernumLabel.innerHTML = apiData.version;
             instaLabel.innerHTML = apiData.instance;
         } else console.error(e);
     }
-    xhr2.send(null);
+    xhr.send(null);
 }
